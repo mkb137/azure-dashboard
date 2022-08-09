62c093ef00d6e1765a675df1f37800e44cfa3a5b
Mike Bennett <mkb137@users.noreply.github.com>
2022-08-08 19:18:23 -0600

    Added actix with a "hello world" endpoint

--- azure-dashboard-server/src/main.rs
+++ azure-dashboard-server/src/main.rs
@@ -1,15 +1,31 @@
 #![allow(dead_code)]
+#![allow(unused_imports)]
 
 use crate::settings::Settings;
+use actix_web::{get, web, App, HttpServer, Responder};
 
 mod settings;
 
-fn main() {
+#[get("/hello/{name}")]
+async fn greet(name: web::Path<String>) -> impl Responder {
+    format!("Hello {name}!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
     // Initialize logging
     log4rs::init_file("log4rs.yaml", Default::default()).unwrap_or(());
     log::debug!(" - loading the configuration file");
     let settings = Settings::new().unwrap();
     log::debug!(" - loading settings = {:?}", settings);
 
-    println!("Hello, world!");
+    // Start the Actix server
+    HttpServer::new(|| {
+        App::new()
+            .route("/hello", web::get().to(|| async { "Hello world" }))
+            .service(greet)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
