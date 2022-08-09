a8380b8c112f3d615c55b0e2c007ace9e166ddd3
Mike Bennett <mkb137@users.noreply.github.com>
2022-08-08 19:06:23 -0600

    Added config file loading

--- azure-dashboard-server/src/main.rs
+++ azure-dashboard-server/src/main.rs
@@ -1,6 +1,15 @@
+#![allow(dead_code)]
+
+use crate::settings::Settings;
+
+mod settings;
+
 fn main() {
     // Initialize logging
     log4rs::init_file("log4rs.yaml", Default::default()).unwrap_or(());
-    log::debug!["Hello2"];
+    log::debug!(" - loading the configuration file");
+    let settings = Settings::new().unwrap();
+    log::debug!(" - loading settings = {:?}", settings);
+
     println!("Hello, world!");
 }
---
+++ azure-dashboard-server/src/settings.rs
@@ -0,0 +1,49 @@
+
+// The settings relating to a single subscription.
+#[derive(Debug, serde::Deserialize)]
+#[allow(unused)]
+pub struct SubscriptionSettings {
+    // The Azure OAuth2 base auth URL
+    token_url: String,
+    // The tenant ID (a GUID)
+    tenant_id: String,
+    // The subscription ID (a GUID)
+    subscription_id: String,
+    // The Azure AD App Registration client ID (a GUID)
+    client_id: String,
+    // The Azure AD App Registration secret value (a long string of random chars)
+    // Note: Not the secret ID (a GUID)
+    // Note2: This secret expires 6, 12, or however many months were specified at time of
+    // creation and will have to be updated.
+    client_secret: String,
+    // The OAuth2 resource name
+    resource: String,
+}
+
+// The application configuration settings.
+#[derive(Debug, serde::Deserialize)]
+#[allow(unused)]
+pub struct Settings {
+    // The subscriptions.
+    subscriptions: Vec<SubscriptionSettings>,
+}
+
+impl Settings {
+
+    // Loads the settings from file.
+    pub fn new() -> Result<Self,config::ConfigError> {
+        log::debug!("Settings.new");
+        // Get the run mode from the environment variables.
+        // Default to "local" if not specified.
+        let run_mode = std::env::var("RUN_MODE").unwrap_or_else(|_| "local".into());
+        log::debug!(" - run_mode = {:?}", run_mode);
+        // Load the settings from configuration
+        let settings = config::Config::builder()
+            // Add the default configuration file
+            .add_source(config::File::with_name("config.json"))
+            // Build the file
+            .build()?;
+        // Try to deserialize the file
+        settings.try_deserialize()
+    }
+}
