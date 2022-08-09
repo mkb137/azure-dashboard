5b700a7c23355dc20464162544442da126c65339
Mike Bennett <mkb137@users.noreply.github.com>
2022-08-08 19:27:45 -0600

    Added a sample token cache as app data

--- azure-dashboard-server/src/main.rs
+++ azure-dashboard-server/src/main.rs
@@ -3,12 +3,19 @@
 
 use crate::settings::Settings;
 use actix_web::{get, web, App, HttpServer, Responder};
+use std::sync::Mutex;
 
 mod settings;
 
+struct AccessTokenCache {
+    counter: Mutex<i32>,
+}
+
 #[get("/hello/{name}")]
-async fn greet(name: web::Path<String>) -> impl Responder {
-    format!("Hello {name}!")
+async fn greet(name: web::Path<String>, data: web::Data<AccessTokenCache>) -> impl Responder {
+    let mut counter = data.counter.lock().unwrap();
+    *counter += 1;
+    format!("Hello {name}! (counter = {counter})")
 }
 
 #[actix_web::main]
@@ -19,9 +26,14 @@ async fn main() -> std::io::Result<()> {
     let settings = Settings::new().unwrap();
     log::debug!(" - loading settings = {:?}", settings);
 
+    // Initialize the token cache
+    let token_cache = web::Data::new(AccessTokenCache {
+        counter: Mutex::new(0),
+    });
     // Start the Actix server
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
+            .app_data(token_cache.clone())
             .route("/hello", web::get().to(|| async { "Hello world" }))
             .service(greet)
     })
